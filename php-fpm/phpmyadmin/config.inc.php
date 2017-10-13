cc782d1ecb70c19ace3e3b8ce2b548661f397b12
Cao Jiayuan <cjy632258@hotmail.com>
2017-10-13 16:35:55 +0800

    fi

--- php-fpm/phpmyadmin/config.inc.php
+++ php-fpm/phpmyadmin/config.inc.php
@@ -14,7 +14,7 @@
  * This is needed for cookie based authentication to encrypt password in
  * cookie. Needs to be 32 chars long.
  */
-$cfg['blowfish_secret'] = '632258'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = 'fMeL64nUtUZw7ROQOoFVnoY23BUuuXdP'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /**
  * Servers configuration
