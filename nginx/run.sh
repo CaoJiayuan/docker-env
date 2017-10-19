0f2d590865a1997b81cf84b0e9d5000ba81a1073
caojiayuan <cjy632258@hotmail.com>
2017-10-19 20:31:28 +0800

    fix

--- nginx/run.sh
+++ nginx/run.sh
@@ -3,7 +3,9 @@ if [ "${UPLOAD_SIZE}" == "" ]; then
    export UPLOAD_SIZE="8M"
 fi
 
-envsubst "$UPLOAD_SIZE" < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+echo "max file size ${UPLOAD_SIZE}"
+
+envsubst '$UPLOAD_SIZE' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 cat /etc/nginx/nginx.conf
 
