f0d48849b20f1f9a0b369be4910c0a2c27ecbd07
Cao Jiayuan <cjy632258@hotmail.com>
2017-10-20 10:56:50 +0800

    fix

--- php-fpm/run.sh
+++ php-fpm/run.sh
@@ -1,5 +1,5 @@
 if [ -f "${SUPERVISOR_CONF}" ]; then
-    cp ${SUPERVISOR_CONF} /etc/supervisor/conf.d/
+    cp ${SUPERVISOR_CONF} /etc/supervisor/default.conf
 fi
 if [ -f "${CRONTAB_FILE}" ]; then
     cp ${CRONTAB_FILE} /etc/crontabs/root
