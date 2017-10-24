43f5316f47be798c0d81e15343c879f5a32a5e32
Cao Jiayuan <cjy632258@hotmail.com>
2017-10-24 10:44:16 +0800

    env

--- nginx/run.sh
+++ nginx/run.sh
@@ -3,13 +3,18 @@ if [ "${UPLOAD_SIZE}" == "" ]; then
    export UPLOAD_SIZE="8M"
 fi
 
+if [ "${BODY_BUFFER_SIZE}" == "" ]; then
+   export BODY_BUFFER_SIZE="128K"
+fi
+
 if [ -f "${CONFIG_FILE}" ]; then
     cp ${CONFIG_FILE} /etc/nginx/default.conf
 fi
 
 echo "max file size ${UPLOAD_SIZE}"
+echo "max body buffer size ${BODY_BUFFER_SIZE}"
 
-envsubst '$UPLOAD_SIZE' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$UPLOAD_SIZE,$BODY_BUFFER_SIZE' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 cat /etc/nginx/nginx.conf
 
